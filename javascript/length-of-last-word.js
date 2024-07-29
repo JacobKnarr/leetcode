70bce1cb3c7e08c252df6c721d8444a9654eab96
JacobKnarr <jacobknarr@gmail.com>
2024-07-29 19:35:13 -0400

    easy(length-of-last-word): return the length of the last word in a string

--- javascript/length-of-last-word.js
+++ javascript/length-of-last-word.js
@@ -1,8 +1,46 @@
+const { runTests } = require("./test/runners");
+
 /**
  * @param {string} s
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    
+    let count = 0;
+    let position = s.length - 1;
+
+    while (position >= 0) {
+        const character = s[position--];
+        if (character === " " && count !== 0) {
+            break;
+        } else if (character !== " ") {
+            count++;
+        }
+    };
+
+    return count;
+
+    // return s.trim().split(" ").pop().length;
 };
 
+const tests = [
+    {
+        params: {
+            input: "Hello World"
+        },
+        output: 5
+    },
+    {
+        params: {
+            input: "   fly me   to   the moon  "
+        },
+        output: 4
+    },
+    {
+        params: {
+            input: "luffy is still joyboy"
+        },
+        output: 6
+    }
+];
+
+runTests(tests, lengthOfLastWord);
